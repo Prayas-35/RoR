519f6928843b5de637bb7c8c3f696b87011b8019
srijansarkar <srijansarkar2705@gmail.com>
2025-03-30 04:07:35 +0000

    game changed

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Providers from "./_providers/rainbowkit";
+import ConvexClientProviders from "@/providers/ConvexClientProviders";
+
+export const metadata: Metadata = {
+  title: "Ruins of Rome",
+  description: "Fight for Blessings",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/logo1.png" />
+      </head>
+      <body className={`antialiased bg-[#000]`}>
+        <ConvexClientProviders>
+          <Providers>{children}</Providers>
+        </ConvexClientProviders>
+      </body>
+    </html>
+  );
+}
---
+++ web3/contracts/celestial.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+/**
+ * @title Celestial
+ * @author RuinsOfRome devs
+ * @dev Celestial is a contract to mint the celestial nfts
+ */
+
+contract Celestial is ERC721URIStorage {
+    /* Custom Errors */
+    error Celestial__OnlyOwner();
+
+    uint256 private _nextTokenId;
+    address public owner;
+
+    constructor() ERC721("Celestial", "GOD") {
+        _nextTokenId = 0;
+        owner = msg.sender;
+    }
+
+    function mintNFT(address to, string memory tokenURI) public {
+        uint256 tokenId = _nextTokenId;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        _nextTokenId++;
+    }
+
+    function getNFTs(address addy) public view returns (uint256[] memory) {
+        if (addy == address(0)) revert("Invalid address");
+        
+        uint256[] memory tokens = new uint256[](_nextTokenId);
+        uint256 tokenCount = 0;
+        
+        for(uint256 i = 0; i < _nextTokenId; i++) {
+            if(ownerOf(i) == addy) {
+                tokens[tokenCount] = i;
+                tokenCount++;
+            }
+        }
+
+        // Create correctly sized array
+        uint256[] memory result = new uint256[](tokenCount);
+        for(uint256 i = 0; i < tokenCount; i++) {
+            result[i] = tokens[i];
+        }
+        
+        return result;
+    }
+}
